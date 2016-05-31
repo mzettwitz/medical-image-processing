ee905bfc69205e31f10e916b821eb30c9834d842
elke94 <elke@grabe-online.de>
2016-05-31 07:41:20 +0200

    some changes

--- Hough.m
+++ Hough.m
@@ -4,15 +4,14 @@ function [y,max_x] = Hough(img)
 %imrotate(img, angle);
 rotI = imrotate(img,0);
 
-<<<<<<< HEAD
+
 %figure, imshow(rotI), title('rotated image');
-=======
+
 %gesch?tzter Grauwert der Nadel
 %p01: 950+-400
 %p02:-200+-700
 nadel= 950;
 v = 400;
->>>>>>> refs/remotes/origin/master
 
 
 %====================================== edge operator
@@ -23,7 +22,7 @@ BW = edge(rotI,'sobel','vertical');
 %figure, imshow(BW), title('edges');
 
 
-<<<<<<< HEAD
+
 %====================================== hough space
 %[K,T,R] = hough(BW,'Theta', -85:0.05:85);
 %K = imresize(K,[400 800]);
@@ -36,9 +35,8 @@ BW = edge(rotI,'sobel','vertical');
 %====================================== hough peaks
 %houghpeaks(houghMatrix, numberOfPeaks,'option',value;
 P = houghpeaks(H,1,'threshold',0.85*max(H(:)));
-=======
 %Verbesserungsm?glichkeiten:
-J = bild;
+J = img;
 %figure, imshow(J);
 
 %K = histeq(bild);
@@ -50,23 +48,23 @@ J = bild;
 rotI=J;
 
 %m?gliche Methoden: Canny,log,zerocross
->>>>>>> refs/remotes/origin/master
+
 
 %figure, imshow(H,[]), title('hough space'), hold on;
 %p_x = theta(P(:,2)); p_y = rho(P(:,1)); plot(p_x,p_y,'s','color','red');
 
-<<<<<<< HEAD
+
 
 %====================================== hough lines
 %houghlines(edgeImg,theta,rho,peaks,'option', value);
 lines = houghlines(BW,theta,rho,P,'FillGap',2.5,'MinLength',4.5);
-=======
+
 [H,theta,rho] = hough(BW);
-P = houghpeaks(H,1,'threshold',ceil(0.7*max(H(:))));
+%P = houghpeaks(H,1,'threshold',ceil(0.7*max(H(:))));
 
 %MinLength erh?hen reduziert Anzahl falscher Kanten
-lines = houghlines(BW,theta,rho,P,'FillGap',3,'MinLength',3);
->>>>>>> refs/remotes/origin/master
+%lines = houghlines(BW,theta,rho,P,'FillGap',3,'MinLength',3);
+
 
 
 %====================================== print lines
@@ -82,15 +80,13 @@ min_x = 0;
 min_y = 0;
 for k = 1:length(lines)
     xy = [lines(k).point1; lines(k).point2];
-<<<<<<< HEAD
-   % plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-   % plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
     %Vektor mit den x bzw. y Koordinaten der Punkte
-=======
+
     plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
    %Vektor mit den x bzw. y Koordinaten der Punkte
->>>>>>> refs/remotes/origin/master
+
     x = [x lines(k).point1(1) lines(k).point2(1)];
     y = [y lines(k).point1(2) lines(k).point2(2)];
 
@@ -121,7 +117,7 @@ for k = 1:length(lines)
        max_x = lines(k).point2(1);
     end
 end
-<<<<<<< HEAD
+
     plot(min_x,min_y,'x', 'Color','blue');
     plot(max_x,max_y,'x', 'Color','green');
     %Berechnen der Euklidischen Distanz
@@ -132,35 +128,31 @@ end
     y2 = polyval(p,t2);
     p1 = polyval(p,max_x);
     % Anzeigen von Gerade und Nadelspitze
-<<<<<<< HEAD
+
     plot(max_x,p1,'o',t2,y2, 'LineWidth',2);
     
 end
-=======
-    plot(max_x,polyval(p,max_x),'o',t2,y2,'LineWidth',2)
-=======
+
+   % plot(max_x,polyval(p,max_x),'o',t2,y2,'LineWidth',2);
+
     
     % Finden des hellsten Punktes (Nadelspitze)
     % Auslassen von Artefakten(p01, Rand)
-    max_bright = max(img(:));
-    [r, c] = find(img == max_bright); 
-    cond_c = c < (size(img,1) * 0.9);
-    pos = max(find(c == max(c(cond_c))));
-    n_x = c(pos); n_y = r(pos);
-    plot(n_x, n_y, 'o', 'Color', 'g')
+  %  max_bright = max(img(:));
+   % [r, c] = find(img == max_bright); 
+   % cond_c = c < (size(img,1) * 0.9);
+   % pos = max(find(c == max(c(cond_c))));
+   % n_x = c(pos); n_y = r(pos);
+   % plot(n_x, n_y, 'o', 'Color', 'g')
     %plot(c, r, 'o', 'Color', 'g')  % all points
     
-    if(max_y <= n_y*1.1)
+  %  if(max_y <= n_y*1.1)
         %Berechnung der Ausgleichsgerade bis zur Nadelspitze
-        p = polyfit(x,y,1);
-        t2 = 0:0.1:max_x;
-        y2 = polyval(p,t2);
+       % p = polyfit(x,y,1);
+       % t2 = 0:0.1:max_x;
+       % y2 = polyval(p,t2);
         % Anzeigen von Gerade und Nadelspitze
-        plot(max_x,polyval(p,max_x),'o',t2,y2, 'LineWidth',2)
-    end
-    
-    
->>>>>>> refs/remotes/origin/testing
-     
-end
->>>>>>> refs/remotes/origin/master
+     %   plot(max_x,polyval(p,max_x),'o',t2,y2, 'LineWidth',2)
+  %  end
+   
+   % end
--- Test.m
+++ Test.m
@@ -4,23 +4,17 @@ clear;
 %Pfad des Ordners, in dem die Dicom-Datein liegen
 dcm_path = ('D:\Studium\16SoSe\MedBV\medbv_data\medbv_data\p01\'); 
 %dcm_path = ('../data/p01/'); 
-<<<<<<< HEAD
-%dcm_path = ('~/Dev/MedBV/data/p02/'); 
-=======
-dcm_path = ('~/Dev/MedBV/data/p01/'); 
->>>>>>> refs/remotes/origin/testing
 % Ansammlung Dicom-Dateien
 filenames  = dir(fullfile(dcm_path, '*.dcm')); 
 % wir brauchen erstmal nur die Namen der Dateien
 filenames = {filenames.name}; 
 % m = Anzahl aller Dateien
-<<<<<<< HEAD
+
 %m = numel(filenames);               
 m = 10;
-=======
-m = 5;%numel(filenames);               
+%numel(filenames);     
+        
 
->>>>>>> refs/remotes/origin/testing
 for k=1:m 
     d = filenames{k}; 
     f = fullfile(dcm_path, d); 
