0df339226babad711ff622de6c0ba40c0a1a1c5b
elke94 <elke@grabe-online.de>
2016-06-14 17:04:16 +0200

    rectangle which is around the needle

--- Hough.m
+++ Hough.m
@@ -87,11 +87,23 @@ end
 
     % Finden des hellsten Punktes (Nadelspitze)
     % Auslassen von Artefakten(p01, Rand)
-    max_bright = max(img(:));
-    [r, c] = find(img == max_bright); 
-    cond_c = c < (size(img,1) * 0.9);
-    pos = find(c(cond_c) == max(c(cond_c)), 1, 'last' );
-    n_x = c(pos); n_y = r(pos);
+    if(min_x < max_x)
+        rect_around = img(min_y:max_y,min_x:max_x);
+    else
+        rect_around = img(min_y:max_y,max_x:min_x);
+    end
+    max_bright = max(rect_around(:));
+    if(img(max_x,max_y)< max_bright *0.8)       
+       % [r, c] = find(img == max_bright); 
+        [r, c] = find(rect_around == max_bright); 
+        cond_c = c < (size(img,1) * 0.9);
+        pos = find(c(cond_c) == max(c(cond_c)), 1, 'last' );
+        n_x = c(pos)+ max_x;
+        n_y = r(pos) + min_y;        
+    else
+        n_x = max_x;
+        n_y = max_y;
+    end
     plot(n_x, n_y, 'o', 'Color', 'g')
     %plot(c, r, 'o', 'Color', 'g')  % all points
     
